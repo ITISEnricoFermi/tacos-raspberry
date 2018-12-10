2fad99fd095c1efb7afa253ea8611c82d399e92f
n4y0n <e.montada0@gmail.com>
2018-12-10 19:12:05 +0100

    Cambiato nome

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Chaner!" });
+  res.render("index", { title: "Api!" });
 });
 
 module.exports = router;

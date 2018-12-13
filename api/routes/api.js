ea0d0326ff14b8e5fe0ffd29b971e689fa21d0ea
Ernesto Montada Romero <e.montada0@gmail.com>
2018-12-13 21:05:47 +0100

    Refactoring.

--- api/routes/api.js
+++ api/routes/api.js
@@ -6,20 +6,4 @@ const router = express.Router();
 
 router.use("/v1", v1);
 
-router.get("/", (req, res) => {
-  res.status(200).send();
-});
-router.post("/", (req, res) => {
-  res.status(200).send();
-});
-router.put("/", (req, res) => {
-  res.status(200).send();
-});
-router.delete("/", (req, res) => {
-  res.status(200).send();
-});
-router.options("/", (req, res) => {
-  res.status(200).send();
-});
-
 module.exports = router;

23751022d18cea4ed7e1a898acf218ffe7f54ae0
Ernesto Montada Romero <e.montada0@gmail.com>
2018-12-16 21:08:50 +0100

    Mi sento meglio

--- api/src/socket/socket.ts
+++ api/src/socket/socket.ts
@@ -3,25 +3,19 @@ import { Server } from "http";
 import { EventBus } from "../config/bus";
 
 export function socket(server: Server): void {
-  const io = socketio(server);
-  let sockets: Array<socketio.Socket> = [];
+  const io = socketio(server, {
+    serveClient: process.env.DEBUG ? true : false
+  });
 
-  // Anti-memory-leak strategy
   EventBus.on("device-state-change", dev => {
-    sockets.forEach(socket => socket.emit("device-state-change", dev));
+    io.sockets.emit("device-state-change", dev);
   });
 
   io.on("connection", socket => {
     socket.emit("READY");
-    sockets.push(socket);
-
     socket.on("disconnecting", reason => {
-      // Non la soluzione piu elegane ma hey funziona
-      sockets = sockets.filter(s => s !== socket);
       console.log("Il socket si sta disconnettendo per:", reason);
-      console.log("Socket connessi:", sockets.length);
     });
-    console.log("Socket connessi:", sockets.length);
   });
 }
 

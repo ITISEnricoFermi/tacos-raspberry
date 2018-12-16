2cc1098d142d97c99ffa02bc7c48fee031231167
Ernesto Montada Romero <e.montada0@gmail.com>
2018-12-16 14:47:28 +0100

    Refactoring dei nomi

--- api/src/socket/socket.ts
+++ api/src/socket/socket.ts
@@ -1,11 +1,11 @@
-import createSocket from "socket.io";
+import socketio from "socket.io";
 import { Server } from "http";
 
 export function socket(server: Server): void {
-  const listener = createSocket(server);
+  const io = socketio(server);
 
-  listener.on("connection", io => {
-    io.emit("READY");
+  io.on("connection", socket => {
+    socket.emit("READY");
   });
 }
 

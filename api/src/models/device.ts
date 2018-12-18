b3fe486791832ccf8c385428b6393d5fe841f4bc
Ernesto Montada Romero <e.montada0@gmail.com>
2018-12-18 19:47:01 +0100

    Soluzione da reimplementare

--- api/src/models/device.ts
+++ api/src/models/device.ts
@@ -1,14 +1,14 @@
-import { Schema, Document, model, Model } from "mongoose";
+import { model, Schema, Model, Document, Query } from "mongoose";
 import IDevice from "../../../Iot-controller/interfaces/IDevice";
 import { DeviceState } from "../../../Iot-controller/interfaces/DeviceState";
 import { DeviceType } from "../../../Iot-controller/interfaces/DeviceType";
 
 export interface IDeviceModel extends IDevice, Document {
   type: DeviceType;
-  changeState(newState: DeviceState): Promise<void>;
+  changeState(newState: DeviceState): Promise<Query<IDeviceModel>>;
 }
 
-const DeviceSchema: Schema = new Schema({
+export const DeviceSchema: Schema = new Schema({
   devid: {
     type: Number,
     required: true
@@ -30,28 +30,10 @@ const DeviceSchema: Schema = new Schema({
 
 DeviceSchema.method("changeState", async function(
   newState: DeviceState
-): Promise<void> {
-  const dev: IDeviceModel = this;
+): Promise<Query<IDeviceModel>> {
+  let dev: IDeviceModel = this;
   return await dev.update({
     state: newState
-  });
-});
-
-DeviceSchema.static("findByDeviceID", function(
-  devid: number
-): Promise<IDeviceModel> {
-  const Device = this;
-  return Device.findOne({
-    devid
-  });
-});
-
-DeviceSchema.static("findByDeviceMac", function(
-  devmac: string
-): Promise<IDeviceModel> {
-  const Device = this;
-  return Device.findOne({
-    devmac
   });
 });
 

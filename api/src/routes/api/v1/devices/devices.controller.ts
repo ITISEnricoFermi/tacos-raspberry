1a6c5d6a4b6aed20be49e21b5c544fe6ad044d05
Ernesto Montada Romero <e.montada0@gmail.com>
2018-12-19 17:12:06 +0100

    Qualche ritocco qua e là

--- api/src/routes/api/v1/devices/devices.controller.ts
+++ api/src/routes/api/v1/devices/devices.controller.ts
@@ -13,6 +13,7 @@ export async function getDevices(req: any, res: any) {
       result: await getAllActiveDevices()
     });
   } catch (e) {
+    e.code = 404;
     handleInternalError(res, e);
   }
 }
@@ -24,6 +25,7 @@ export async function getDevice(req: any, res: any) {
       result: (await findDeviceById(parseInt(req.params.id))).toJSON()
     });
   } catch (e) {
+    e.code = 404;
     handleInternalError(res, e);
   }
 }
@@ -36,30 +38,22 @@ export async function changeState(req: any, res: any) {
     );
     res.json({ state: 200, result: "Ok" });
   } catch (e) {
-    switch (e.code) {
-      case 404:
-        res.status(404).json({
-          status: 404,
-          result: "Device not found"
-        });
-        break;
-      default:
-        handleInternalError(res, e);
-    }
+    e.code = 404;
+    handleInternalError(res, e);
   }
 }
 
 function handleInternalError(res: any, e: any) {
   if (res.app.get("env") === "development") {
-    res.status(500).json({
-      status: 500,
-      message: e.message,
+    res.status(e.code).json({
+      status: e.code,
+      result: e.message,
       "stack-trace": e.stack
     });
   } else {
-    res.status(500).json({
-      status: 500,
-      message: "Oups, something went wrong!"
+    res.status(e.code).json({
+      status: e.code,
+      result: e.message
     });
   }
 }

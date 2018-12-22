3d664691b12bfac0b7005f70f0dfb86545e60937
Ernesto Montada Romero <n4y0n@users.noreply.github.com>
2018-12-22 10:21:11 +0100

    Readme aggiornato e rimosso in-memory-database(#12)
    * Token salt dal file di configurazione
    
    * Rimossa dipendenza body-parser
    
    * Aggiunta proprietà "mac"
    
    * Schema del "device" da salvare nel database
    
    * Aggiunta type information
    
    * Più debug output in dev mode
    
    * Soluzione da reimplementare
    
    * Mockup di qualche device
    
    * Risolto problema #10
    
    * IDevice from IDeviceModel
    
    * Rimosso tutto ciò che ha a che fare con autenticazione (momentaneamente)
    
    * Qualche ritocco qua e là
    
    * Rimosso in-memory-database e (cat | nc -u 255.255.255.255 51966)
    
    * Update README.md
    
    * Update README.md

--- api/src/config/conf.ts
+++ api/src/config/conf.ts
@@ -1,5 +1,4 @@
 export const config = {
-  in_memory_db: false,
   mongo_uri: process.env.MONGOURI || "mongodb://127.0.0.1:27017/db",
   server_port: process.env.PORT || "3000",
   node_env: process.env.NODE_ENV || "development"
--- bin/index.ts
+++ bin/index.ts
@@ -14,3 +14,20 @@ export { server };
 setInterval(() => {
   EventBus.emit("device-state-change", { id: Math.random() });
 }, 1000);
+
+// Dgram socket listener
+import { createSocket, Socket } from "dgram";
+
+const UDP_PORT: number = 0xcafe;
+
+const udpsocket: Socket = createSocket("udp4");
+
+udpsocket.on("close", () => console.log("Socket closed."));
+udpsocket.on("error", e => console.log(e.message));
+udpsocket.on("listening", () => {
+  udpsocket.setBroadcast(true);
+  console.log(`Socket listening for broadcast messages on port ${UDP_PORT}`);
+});
+udpsocket.on("message", m => console.log(`Recieved message: ${m}`));
+
+udpsocket.bind(UDP_PORT);

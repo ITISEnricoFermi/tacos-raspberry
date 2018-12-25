88e1f78932c952838ba7292b7c9d53b357c1016a
PC\Dav <davide.gabrielli_@libero.it>
2018-12-25 22:13:49 +0100

    Udp test

--- udp/udpsocket.ts
+++ udp/udpsocket.ts
@@ -1,66 +1,65 @@
 import { createSocket, Socket } from "dgram";
 
-const UDP_PORT: number = 0xcafe;
+const RecPORT: number = 0xbeaf;
+const DestPORT: number = 0xcafe;
 const udpsocket: Socket = createSocket("udp4");
 
-
 udpsocket.on("close", () => {
-    console.log("Socket closed.");
+  console.log("Socket closed.");
 });
 
-udpsocket.on("error", (e) => {
-    console.log(e.message);
+udpsocket.on("error", e => {
+  console.log(e.message);
 });
 
 udpsocket.on("listening", () => {
   udpsocket.setBroadcast(true);
-  console.log(udpsocket.address())
-  console.log(`Socket listening for broadcast messages on port ${UDP_PORT}`);
+  console.log(udpsocket.address());
+  console.log(`Socket listening for broadcast messages on port ${RecPORT}`);
 });
 
-udpsocket.on("message", (m) =>{
-    try{
-        let data = JSON.parse(m.toString());
-        switch(data.operation)
-        {
-            case "NEW":
-                break;
-            case "UPDATE":
-                break;
-            case "ALIVE":
-                break;
-        }
+udpsocket.on("message", m => {
+  try {
+    let data = JSON.parse(m.toString());
+    switch (data.operation) {
+      case "NEW":
+        break;
+      case "UPDATE":
+        break;
+      case "ALIVE":
+        break;
     }
-    catch(e){
-        console.log("Error on message: "+e+"\n"+m)
-    }
+  } catch (e) {
+    console.log("Error on message: " + e + "\n" + m);
+  }
 });
 
+function sendData(type: string, mac: string, payload: string) {
+  let typeB: Buffer = Buffer.from(type);
+  let macB: Buffer = Buffer.from(mac.replace(/:/g, ""), "hex");
+  let payloadB: Buffer = Buffer.from(payload);
+  let lenB: Buffer = new Buffer(1);
+  lenB.writeInt8(payload.length, 0);
 
-function sendData(payload, type, mac) {
-    type = Buffer.from(type);
-    mac = Buffer.from(mac);
-    payload = Buffer.from(payload);
-    let len = new Buffer(1);
-    len.writeInt8(payload.length,0)
+  if (
+    typeB.length > 1 ||
+    macB.length > 6 ||
+    lenB.length > 1 ||
+    payloadB.length > 255
+  )
+    return Error("Invalid length for arguments");
 
-    if(type.length>8||mac.length>6*8||len.length>8||payload.length>255*8)
-        return Error("Invalid length for arguments")
-
-    console.log("Sending...")
-    var message = Buffer.concat([type, mac, len, payload]);
-    udpsocket.send(message, 50743, "255.255.255.255", (err) => {
-        if (err) return console.log(err);
-    });
+  console.log("Sending...");
+  let message: Buffer = Buffer.concat([typeB, macB, lenB, payloadB]);
+  udpsocket.send(message, DestPORT, "255.255.255.255", err => {
+    if (err) return console.log(err);
+  });
 }
 
-udpsocket.bind(UDP_PORT);
+udpsocket.bind(RecPORT);
 
-setInterval(()=>{
-    sendData("Roba","NEW","SO:NO:IO:LO:GI:UR:OO")
-},2000);
+setInterval(() => {
+  sendData("N", "AA:BB:CC:11:22:33", "Roba");
+}, 2000);
 
-export {
-    udpsocket,
-    sendData
-};
+export { udpsocket, sendData };

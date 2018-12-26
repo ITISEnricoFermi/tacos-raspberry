b5d9922ead8e88a89e0afd73e71e06634271ad7e
PC\Dav <davide.gabrielli_@libero.it>
2018-12-26 18:44:54 +0100

    Broadcast address calculator

--- config/conf.ts
+++ config/conf.ts
@@ -2,5 +2,6 @@ export const config = {
   mongo_uri: process.env.MONGOURI || "mongodb://127.0.0.1:27017/db",
   server_port: process.env.PORT || "3000",
   node_env: process.env.NODE_ENV || "development",
-  udp_port: process.env.DEV_PORT || (0xcafe).toString()
+  udp_rec_port: Number(process.env.REC_PORT) || 0xcafe,
+  udp_dest_port: Number(process.env.DEST_PORT) || 0xbeef
 };
--- udp/udpsocket.ts
+++ udp/udpsocket.ts
@@ -1,7 +1,10 @@
 import { createSocket, Socket } from "dgram";
+import { config } from "../config/conf";
+import os, { NetworkInterfaceInfo } from "os";
 
-const RecPORT: number = 0xbeaf;
-const DestPORT: number = 0xcafe;
+const RecPORT: number = config.udp_rec_port;
+const DestPORT: number = config.udp_dest_port;
+var bcAddress: string;
 const udpsocket: Socket = createSocket("udp4");
 
 udpsocket.on("close", () => {
@@ -14,7 +17,6 @@ udpsocket.on("error", e => {
 
 udpsocket.on("listening", () => {
   udpsocket.setBroadcast(true);
-  console.log(udpsocket.address());
   console.log(`Socket listening for broadcast messages on port ${RecPORT}`);
 });
 
@@ -39,7 +41,7 @@ function sendData(type: string, mac: string, payload: string) {
   let macB: Buffer = Buffer.from(mac.replace(/:/g, ""), "hex");
   let payloadB: Buffer = Buffer.from(payload);
   let lenB: Buffer = new Buffer(1);
-  lenB.writeInt8(payload.length, 0);
+  lenB.writeInt8(payloadB.length, 0);
 
   if (
     typeB.length > 1 ||
@@ -51,11 +53,51 @@ function sendData(type: string, mac: string, payload: string) {
 
   console.log("Sending...");
   let message: Buffer = Buffer.concat([typeB, macB, lenB, payloadB]);
-  udpsocket.send(message, DestPORT, "255.255.255.255", err => {
+  udpsocket.send(message, DestPORT, bcAddress, err => {
     if (err) return console.log(err);
   });
 }
 
+function calculateBroadcast() {
+  var ifaces: {
+    [index: string]: NetworkInterfaceInfo[];
+  } = os.networkInterfaces();
+  Object.keys(ifaces).forEach(function(ifname) {
+    ifaces[ifname].forEach(function(iface) {
+      if (bcAddress) return;
+      if ("IPv4" !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+      let address: string[] = iface.address.split(".");
+      let netmask: string[] = iface.netmask.split(".");
+      let broadcast: string[] = [];
+      for (let i = 0; i < 4; i++) {
+        let byteAddress: string = Number(address[i]).toString(2);
+        let byteNetMask: string = Number(netmask[i]).toString(2);
+        let notbyteNetMask: string[] = [];
+        //invert
+        if (byteNetMask.length < 8) {
+          for (let j = 0; j < 8 - byteNetMask.length; j++) {
+            notbyteNetMask.push("1");
+          }
+        }
+        for (let j = 0; j < byteNetMask.length; j++) {
+          notbyteNetMask.push(
+            byteNetMask[j] == "0" || byteNetMask[j] == "0" ? "1" : "0"
+          );
+        }
+        let byteBroadcast: number;
+        byteBroadcast =
+          parseInt(byteAddress, 2) | parseInt(notbyteNetMask.join(""), 2);
+        broadcast.push(byteBroadcast.toString());
+      }
+      bcAddress = broadcast.join(".");
+      console.log("Broadcast address: " + bcAddress);
+    });
+  });
+}
+calculateBroadcast();
 udpsocket.bind(RecPORT);
 
 setInterval(() => {

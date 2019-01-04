b0ff24fcdd67b2644200bc175d9e1c8a3a58cf56
Ernesto Montada Romero <e.montada0@gmail.com>
2019-01-04 14:12:53 +0100

    namespaced loggers

--- config/log.ts
+++ config/log.ts
@@ -17,40 +17,32 @@ export namespace FDLogger {
     return `[${info.timestamp}] [${info.level}] ${info.label}: ${info.message}`;
   });
 
-  const _label = label({ label: "main" });
-  const _timestamp = timestamp();
-
-  const rootLogger: winston.Logger = createLogger({
-    level: LogLevel.silly,
-    format: combine(_label, _timestamp, myFormat),
-    transports: [
-      new transports.Console({
-        level: config.log_level || LogLevel.debug,
-        format: combine(colorize(), _label, _timestamp, myFormat)
-      }),
-      new transports.File({
-        level: LogLevel.silly,
-        filename: config.log_file || "logfile.log"
-      }),
-      new transports.File({
-        level: LogLevel.error,
-        filename: config.log_err_file || "errlog.log"
-      })
-    ]
-  });
-
   function createWinstoneLogger(name: string): winston.Logger {
-    return rootLogger; /*.child({
-      format:
-    });*/
+    const _label = label({ label: name });
+    const _timestamp = timestamp();
+
+    return createLogger({
+      level: config.log_level || LogLevel.silly,
+      format: combine(_label, _timestamp, myFormat),
+      transports: [
+        new transports.Console({
+          level: config.log_level || LogLevel.debug,
+          format: combine(colorize(), _label, _timestamp, myFormat)
+        }),
+        new transports.File({
+          level: LogLevel.silly,
+          filename: config.log_file || "logfile.log"
+        }),
+        new transports.File({
+          level: LogLevel.error,
+          filename: config.log_err_file || "errlog.log"
+        })
+      ]
+    });
   }
 
   export function getLogger(name: string): winston.Logger {
     return createWinstoneLogger(name);
-  }
-
-  export function getRootLogger() {
-    return rootLogger;
   }
 }
 import getLogger = FDLogger.getLogger;

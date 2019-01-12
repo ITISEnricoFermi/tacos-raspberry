ddc4f6f1bce743f086408a2598594f25c45ea11b
Ernesto Montada Romero <e.montada0@gmail.com>
2019-01-12 16:18:51 +0100

    Aggiunto script di building per client e server su windows

---
+++ build.bat
@@ -0,0 +1,38 @@
+@echo off
+
+echo This operation should be done only the first time you clone this repo or if something changes in the client repo.
+
+echo Setting up client submodule...
+
+echo  Init submodule
+call git submodule init
+
+echo  Done
+echo Clone submodule
+
+call git submodule update
+
+echo Done
+
+echo Building server...
+
+call npm install
+call npm run build
+
+echo Done
+
+cd api\client
+
+echo Building client...
+
+call npm install
+call npm run build
+
+echo Done
+echo Copying client in public section of server.
+
+cd ..\..
+move api\client\dist dist\api\public
+
+echo Done
+echo Run 'npm run dev' or 'npm start' to launch the server
